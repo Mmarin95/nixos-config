fa36ccffbb47b030ae70cb76ea2451a57f64355c
Mmarin95 <9479635+Mmarin95@users.noreply.github.com>
2024-06-06 19:36:45 +0200

    chore: Move environment variables and pulse audio

--- modules/system.nix
+++ modules/system.nix
@@ -11,6 +11,9 @@
     ];
   };
 
+  ## ENVIRONMENT
+  environment.variables.EDITOR = "nvim";
+
   ## XSERVER AND GNOME
 
   # Enable the X11 windowing system.
@@ -49,4 +52,24 @@
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
+
+  ## SOUND
+  # Enable sound with pipewire.
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
 }
